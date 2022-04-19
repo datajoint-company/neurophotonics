0d2c2026385b42f686c55422eae962f3be34ad90
ernaldis <tim.chandler@datajoint.com>
2022-04-19 15:42:45 -0500

    change pop_fields to not display progress

--- scripts/pop_fields.py
+++ scripts/pop_fields.py
@@ -1,4 +1,4 @@
 from neurophotonics.fields import *
 
-EField.populate(reserve_jobs=True, processes = 1024, display_progress=True)
-DField.populate(reserve_jobs=True, processes = 1024, display_progress=True)
+EField.populate(reserve_jobs=True, processes = 1024, display_progress=False)
+DField.populate(reserve_jobs=True, processes = 1024, display_progress=False)
