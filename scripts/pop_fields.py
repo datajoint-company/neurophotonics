0eae33e2c254b91e6f96725f2d5831b7e3588fa7
Timothy Chandler <ernaldis@protonmail.com>
2022-04-26 16:30:23 -0500

    leverate multiproccessing in later stages

--- scripts/pop_fields.py
+++ scripts/pop_fields.py
@@ -1,14 +1,4 @@
 from neurophotonics.fields import *
 
-
 EField.populate(reserve_jobs=True, processes=1024, display_progress=False)
 DField.populate(reserve_jobs=True, processes=1024, display_progress=False)
-# try:
-#    EField.populate(reserve_jobs=True, processes = 1024, display_progress=False)
-# except ValueError:
-#    print('EField already populated')
-#
-# try:
-#    DField.populate(reserve_jobs=True, processes = 1024, display_progress=False)
-# except ValueError:
-#    print('DField already populated')
