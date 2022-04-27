2e45e3d81d3c5d15d33db89ab9916e9fe0ac65b9
ec2 container <service-health@datajoint.com>
2022-04-27 17:19:24 +0000

    switch from docker to podman compose

--- startup.sh
+++ startup.sh
@@ -2,12 +2,12 @@
 
 echo "Setting up..."
 git checkout master
-docker-compose -f ./docker/docker-compose-dev.yaml down
+podman-compose -f ./docker/docker-compose-dev.yaml down
 git pull origin master
 
 echo "Starting run..."
 start_time=$(date +"%Y-%m-%d_%H:%M:%S")
-WORKER_COUNT=1 HOST_UID=$(id -u) docker-compose -f ./docker/docker-compose-dev.yaml up --build
+WORKER_COUNT=1 HOST_UID=$(id -u) podman-compose -f ./docker/docker-compose-dev.yaml up --build
 
 echo "Saving logs..."
 echo "$(podman-compose -f ./docker/docker-compose-dev.yaml logs)" > log_${start_time}.txt
@@ -18,7 +18,7 @@ git push origin logs
 
 echo "Ending run..."
 git checkout master
-docker-compose -f ./docker/docker-compose-dev.yaml down
+podman-compose -f ./docker/docker-compose-dev.yaml down
 
 echo "Shutting down..."
 sudo poweroff
