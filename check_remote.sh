80e6853a42fa97be0c608af26307dc6f57b64bc3
ernaldis <tim.chandler@datajoint.com>
2022-04-29 12:06:55 -0500

    fix check_remote return values

--- check_remote.sh
+++ check_remote.sh
@@ -4,7 +4,7 @@ str1=$(git remote show origin | grep pushes\ to\ master)
 str2="    master    pushes to master    (up to date)"
  
 if [ "$str1" != "$str2" ]; then
-    echo "Both Strings are not Equal."
+    echo "False"
 else
-    echo "Both Strings are Equal."
+    echo "True"
 fi
