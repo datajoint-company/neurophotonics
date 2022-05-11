2049d64ced8efcbe2df61c4a7abcf6e2ce9de86d
Timothy Chandler <tim.chandler@datajoint.com>
2022-05-11 09:04:25 -0500

    Update check_remote.sh
    simplify script to one line
    
    Co-authored-by: Raphael Guzman <38401847+guzman-raphael@users.noreply.github.com>

--- check_remote.sh
+++ check_remote.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
- 
-str1=$(git ls-remote origin | grep HEAD | awk '{print $1;}')
-str2=$(git rev-parse HEAD)
- 
-if [ "$str1" != "$str2" ]; then
-    echo "False"
-else
-    echo "True"
-fi
+[ "$(git rev-parse HEAD)" = "$(git ls-remote origin | grep HEAD | awk '{print $1;}')" ]
