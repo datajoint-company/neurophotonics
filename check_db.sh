a6dd16cd123d9c798cf2e20191334291806aa0a7
Timothy Chandler <tim.chandler@datajoint.com>
2022-05-11 08:46:28 -0500

    Update check_db.sh
    switch spacing syntax
    
    Co-authored-by: Raphael Guzman <38401847+guzman-raphael@users.noreply.github.com>

--- check_db.sh
+++ check_db.sh
@@ -1,6 +1,6 @@
 keys=0
 podman-compose -f ./docker/docker-compose-dev.yaml logs 2> /dev/null |\
-  grep -E '^keys[[:space:]]+processed:[[:space:]]+[0-9]+$' |\
+  grep -E '^keys\s+processed:\s*[0-9]+$' |\
   while read line ; 
     do keys=$(( $keys + ${line:16} )) ; 
   done
