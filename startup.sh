9051cd6a02f975ce7bf02badd81a869ed5e118d0
Timothy Chandler <tim.chandler@datajoint.com>
2022-05-11 11:37:36 -0500

    Update startup.sh
    simplify boolean logic & shorten lines

--- startup.sh
+++ startup.sh
@@ -6,11 +6,13 @@ podman-compose -f ./docker/docker-compose-dev.yaml down
 start_time=$(date +"%Y-%m-%d_%H:%M:%S")
 
 echo "Starting run..."
-until [ $([ "$(git rev-parse HEAD)" = "$(git ls-remote origin | grep HEAD | awk '{print $1;}')" ]) && $(bash check_db.sh) = '0']
+until [ "$(git rev-parse HEAD)" = "$(git ls-remote origin | grep HEAD | awk '{print $1;}')" \
+       && $(bash check_db.sh) = '0']
 do
   podman-compose -f ./docker/docker-compose-dev.yaml down
   git pull origin master
-  WORKER_COUNT=1 HOST_UID=$(id -u) podman-compose -f ./docker/docker-compose-dev.yaml up --build >> log_${start_time}.txt 2>&1
+  WORKER_COUNT=1 HOST_UID=$(id -u) podman-compose -f ./docker/docker-compose-dev.yaml up --build \
+    >> log_${start_time}.txt 2>&1
 done
 
 echo "Saving logs..."
