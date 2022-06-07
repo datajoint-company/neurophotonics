ebaa5983fb1bc420fcdbf0521ca511e3c63b765f
ernaldis <ernaldis@protonmail.com>
2022-06-07 10:30:19 -0500

    fix whitespace

--- startup.sh
+++ startup.sh
@@ -7,23 +7,23 @@ start_time=$(date +"%Y-%m-%d_%H:%M:%S")
 
 echo "Starting run..."
 until [ "$(git rev-parse HEAD)" = "$(git ls-remote origin | grep HEAD | awk '{print $1;}')" \
-       && $(bash check_db.sh) = '0']
+	&& $(bash check_db.sh) = '0']
 do
-  podman-compose -f ./docker/docker-compose_standard_worker.yaml -p neurophotonics_standard down
-  git pull origin master
-  WORKER_COUNT=1 HOST_UID=$(id -u) podman-compose -f ./docker/docker-compose_standard_worker.yaml -p neurophotonics_standard up --build \
-    >> log_${start_time}.txt 2>&1
-done
+	podman-compose -f ./docker/docker-compose_standard_worker.yaml -p neurophotonics_standard down
+	git pull origin master
+	WORKER_COUNT=1 HOST_UID=$(id -u) podman-compose -f ./docker/docker-compose_standard_worker.yaml -p neurophotonics_standard up --build \
+		>> log_${start_time}.txt 2>&1
+	done
 
-echo "Saving logs..."
-git checkout logs
-git add log_${start_time}.txt
-git commit -m "Add log for run on ${start_time}"
-git push origin logs
+	echo "Saving logs..."
+	git checkout logs
+	git add log_${start_time}.txt
+	git commit -m "Add log for run on ${start_time}"
+	git push origin logs
 
-echo "Ending run..."
-git checkout master
-podman-compose -f ./docker/docker-compose_standard_worker.yaml -p neurophotonics_standard down
+	echo "Ending run..."
+	git checkout master
+	podman-compose -f ./docker/docker-compose_standard_worker.yaml -p neurophotonics_standard down
 
-echo "Shutting down..."
-sudo poweroff
+	echo "Shutting down..."
+	sudo poweroff
